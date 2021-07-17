c249af6a51022fe97037f2d72522ce5e027358ca
Khushi Kothari <khushi@Khushis-MacBook-Air.local>
2021-07-18 01:35:06 +0530

    Made this awesome website!

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
-import { Carousel } from 'react-responsive-carousel';
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div>
-
     {/* Hero Section */}
     <div className="hero_bg">
     <div className="">
@@ -15,58 +17,73 @@ export default function Home() {
       </Head>
 
       
-      <header className="text-white body-font w-full h-12">
+      <header className="text-gray-800 body-font w-full h-12">
       <div className="container mx-auto flex flex-wrap justify-between p-5 flex-col md:flex-row">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 mr-5">
         {/* Logo */}
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full" viewBox="0 0 24 24">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-yellow-600 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span className="ml-3 text-xl text-white">Bluebell Industries</span>
+          <span className="ml-3 text-2xl font-bold text-gray-900">Bluebell Industries</span>
         </a>
-        <div className="hidden md:flex flex-row items-center">
+        <div className=" pl-14 md:flex flex-row items-center">
         <nav className="flex lg:w-full flex-wrap items-center text-base md:ml-auto">
           <a className="mr-5 hover:text-gray-900">About</a>
           <a className="mr-5 hover:text-gray-900">Where to buy</a>
           <a className="mr-5 hover:text-gray-900">Contact Us</a>
-        </nav>
-        <button className="inline-flex items-center bg-yellow-600 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 text-white">Login / Signup
+          <button className="flex max-w-max items-center bg-yellow-600 border-0 py-1 px-3 focus:outline-none hover:bg-yellow-700 rounded text-base md:mt-0 text-white"> Signup
         </button>
+        </nav>
+        
         </div>
-        {/* Mobile Logo */}
+
+      {/*
+        {/* Mobile Logo 
         <div className="md:hidden flex items-center absolute top-8 right-8">
-          <button className="mobile-menu-button">
+          <button className="mobile-menu-button" isOpen={isOpen} toggle={() => setOpen(!isOpen)}>
             <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap = "round" strokeLinejoin="round" strokeWidth= "2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
-        </div>
-        {/* Mobile Menu */}
-        <div className="mobile-menu hidden">
+          {isOpen && true (
+        <div className="mobile-menu">
         <div className="md:hidden p-3 bg-gray-300 flex flex-col"></div>
           <a className="block py-1 px-4 text-sm hover:bg-gray-400">About</a>
           <a className="block py-1 px-4 text-sm hover:bg-gray-400">Where to buy</a>
           <a className="block py-1 px-4 text-sm hover:bg-gray-400">Contact Us</a>
-        </div>
-        </div>
+        </div>)}
+        </div> 
+
+        {/* Mobile Menu
+
+        <div className="md:hidden flex items-center absolute top-8 right-8">
+          <button className="mobile-menu-button" isOpen={isOpen} toggle={() => setOpen(!isOpen)}>
+            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap = "round" strokeLinejoin="round" strokeWidth= "2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div> */}
+
+        
+        </div> 
     </header>
 
-      <main className="flex flex-col items-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center w-full flex-1 px-20 text-center h-3/5">
         <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          <div className="text-center lg:w-2/3 w-full p-20 bg-black bg-opacity-50">
-            <h1 className="title-font mb-10 text-5xl font-bold text-white">
+        <div className="container max-w-4xl mx-auto flex px-5 py-36 items-center justify-center flex-col">
+          <div className="text-center">
+            <h1 className="title-font mb-10 text-5xl font-bold text-black">
               Welcome to {' '}
-              <a className="text-blue-400" href="#">
+              <a className="text-yellow-700" href="#">
                 BlueBell
               </a>
             </h1>
-            <p className="mb-8 leading-relaxed text-white">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
+            {/*<p className="mb-8 leading-relaxed text-white">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>*/}
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
+              <button className="inline-flex text-white bg-yellow-600 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-700 rounded text-lg">Button</button>
             </div>
           </div>
-          <img className="lg:w-2/6 mt-8 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded text-right" alt="toothbrush" src="" />
+          {/*<img className="lg:w-2/6 mt-8 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded text-right" alt="toothbrush" src="" />*/}
           </div>
         </section>
       </main>
@@ -76,16 +93,18 @@ export default function Home() {
 
       {/* Scroll */}
       <div className="bg relative">
-      <div className="scroll_bg">
-        <h1 className="title-font mb-10 text-4xl font-bold text-gray-800 justify-center items-center w-52 absolute bottom-40 right-28">Brushing gets better when you Bluebell</h1>
-      </div>
+        <h1 className="title-font mb-10 text-4xl font-bold text-gray-900 justify-center items-center w-80 absolute bottom-32 right-28 bg-white bg-opacity-60 p-6">Brushing gets better when you {' '}
+              <a className="text-yellow-600 text-6xl" href="#">
+                BlueBell
+              </a></h1>
       </div>
 
-      {/* Bristles Products */}
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto flex flex-wrap">
-          <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
-            <div className="brush_img object-cover object-center h-full w-full" />
+      {/* Bristle Products */}
+      <section className="text-gray-600 body-font bg-gray-200 bg-opacity-40">
+      <h1 className="title-font sm:text-4xl text-3xl pt-24 mb-10 font-medium text-gray-900 text-center">What can you expect from our services?</h1>
+        <div className="container px-5 pb-24 mx-auto flex flex-wrap">
+        <div className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+        <img className="object-cover object-center rounded" alt="hero" src="https://c1.wallpaperflare.com/preview/701/370/408/toothbrush-brush-rotten-blue.jpg" />
           </div>
           <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
             <div className="flex flex-col mb-10 lg:items-start items-center">
@@ -149,7 +168,7 @@ export default function Home() {
 
       {/* Aluminium Filament */}
       <div>
-      <section className="text-gray-600 body-font">
+      <section className="text-gray-600 body-font bg-gray-200 bg-opacity-40">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
@@ -158,11 +177,11 @@ export default function Home() {
             <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+            <img className="object-cover object-center rounded" alt="hero" src="https://lh3.googleusercontent.com/proxy/D3GPdd-1gsgLQ2Ss77ovvOvxGlS6SgJogw_jk_uKdCdLtWYDVwDqKTJoo53PkzQZFLlON8Yc3DNwQfTfvyC74kZbLuiZ8YVGzupuyQ63GI6OUzZhXFb-Rw6EuH_aEvFtoFsh6vWn77H1Jb2A7l0r7ytHrWMRfETEAdSRkMgGHDvuBy9EFm2Rt5cgM-29rENBvwFrSTb0BQLDYQ" />
+            {/*<div className="coil object-cover object-center h-full w-full" />*/}
           </div>
         </div>
       </section>
@@ -172,7 +191,7 @@ export default function Home() {
       <div>
         <section className="text-gray-600 body-font relative">
             <div className="absolute inset-0 bg-gray-300">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3777264.7681065374!2d69.07544377746385!3d22.401340698493023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3959051f5f0ef795%3A0x861bd887ed54522e!2sGujarat%2C%20India!5e0!3m2!1sen!2sus!4v1626518989595!5m2!1sen!2sus" width="100%" height="800" style={{border:"0"}} allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3777264.7681065374!2d69.07544377746385!3d22.401340698493023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3959051f5f0ef795%3A0x861bd887ed54522e!2sGujarat%2C%20India!5e0!3m2!1sen!2sus!4v1626518989595!5m2!1sen!2sus" width="100%" height="832" style={{border:"0"}} allowfullscreen="" loading="lazy"></iframe>
             </div>
             <div className="container px-5 py-24 mx-auto flex">
                 <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
@@ -199,52 +218,18 @@ export default function Home() {
 
         {/* Footer */}
         <footer className="text-gray-600 body-font">
-          <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+          <div className="container px-5 py-20 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
             <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
               <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                   <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                 </svg>
-                <span className="ml-3 text-xl">Tailblocks</span>
+                <span className="ml-3 text-xl">Bluebell</span>
               </a>
-              <p className="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+              {/*<p className="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>*/}
             </div>
-            <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+            <div className="pl-20 flex-grow flex md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center justify-evenly">
               <div clasNames="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-                <nav className="list-none mb-10">
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">First Link</a>
-                  </li>
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-                  </li>
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-                  </li>
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                  </li>
-                </nav>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
-                <nav className="list-none mb-10">
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">First Link</a>
-                  </li>
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-                  </li>
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-                  </li>
-                  <li>
-                    <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                  </li>
-                </nav>
-              </div>
-              <div className="lg:w-1/4 md:w-1/2 w-full px-4">
                 <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
                 <nav className="list-none mb-10">
                   <li>
