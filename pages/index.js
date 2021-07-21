bd22a5764101a2f31ef60279f43baec5a30a9cc4
Khushi Kothari <khushi@Khushis-MacBook-Air.local>
2021-07-21 12:39:59 +0530

    made some changes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import { useState } from 'react';
+import Image from "next/image"
 
 //media query min-w-x means apply changes on screen greater than or equal to x
 
 export default function Home() {
 
   const [isOpen, setOpen] = useState(false);
+  const blue = require('../.next/static/image/images/blue.4ad5530941462b7062b5b501b236a917.jpg');
+
 
   return (
     <div>
@@ -104,18 +107,8 @@ export default function Home() {
       <div className="lg:w-4/6 mx-auto">
     
       <div className="flex flex-col sm:flex-row mt-10">
-        <div className="pb-10 sm:w-1/3 text-center sm:pr-8 sm:py-8">
-          <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-              <circle cx="12" cy="7" r="4"></circle>
-            </svg>
-          </div>
-          <div className="flex flex-col items-center text-center justify-center">
-            <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">Phoebe Caulfield</h2>
-            <div className="w-12 h-1 bg-yellow-700 rounded mt-2 mb-4"></div>
-            <p className="text-base">Raclette knausgaard hella meggs normcore williamsburg enamel pin sartorial venmo tbh hot chicken gentrify portland.</p>
-          </div>
+        <div className="mx-auto md:mr-20 my-auto">
+         <Image src = "/blue2.jpg" width="200" height="250" />
         </div>
         <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
           <p className="leading-relaxed text-lg text-justify mb-4">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
@@ -284,19 +277,19 @@ export default function Home() {
                 </nav>
               </div>
               <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">PRODUCTS</h2>
                 <nav className="list-none mb-10">
                   <li>
-                    <a className="text-gray-600 hover:text-gray-800 cursor-pointer">First Link</a>
+                    <a className="text-gray-600 hover:text-gray-800 cursor-pointer">Nylon Bristles</a>
                   </li>
                   <li>
-                    <a className="text-gray-600 hover:text-gray-800 cursor-pointer">Second Link</a>
+                    <a className="text-gray-600 hover:text-gray-800 cursor-pointer">Nylon Bristles</a>
                   </li>
                   <li>
-                    <a className="text-gray-600 hover:text-gray-800 cursor-pointer">Third Link</a>
+                    <a className="text-gray-600 hover:text-gray-800 cursor-pointer">Aluminium Filaments</a>
                   </li>
                   <li>
-                    <a className="text-gray-600 hover:text-gray-800 cursor-pointer">Fourth Link</a>
+                    <a className="text-gray-600 hover:text-gray-800 cursor-pointer">Aluminium Filaments</a>
                   </li>
                 </nav>
               </div>
