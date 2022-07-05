4af38f620d9c4ffdab8bbc73b055651bdc7db397
Khushi Kothari <62203565+khushi-kothari@users.noreply.github.com>
2022-07-05 18:49:23 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -207,7 +207,7 @@ export default function Home() {
             </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img className="object-cover object-center rounded" alt="aluminium filament" src="https://3dk.berlin/919-large_default/pla-filament-aluminium-metallic.jpg" />
+            <img className="object-cover object-center rounded" alt="aluminium filament" src="./images/aluminium.jpeg" />
             {/*<div className="coil object-cover object-center h-full w-full" />*/}
           </div>
         </div>
